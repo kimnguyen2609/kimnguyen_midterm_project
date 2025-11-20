585247be338befc8029ed30b6d7b6cb58d18333e
Kim Nguyen <kimnguyen@stu.feitian.edu>
2025-11-20 21:38:48 +0000

    Initial midterm project commit

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+log_path = os.getenv("LOG_PATH", "/logs")
+
+@app.route('/')
+def home():
+    return f"Updated Flask backend of kimnguyen!"
+
+@app.route('/log')
+def write_log():
+    with open(f"{log_path}/kimnguyen_access.log", "a") as f:
+        f.write("Flask route /log was accessed\n")
+    return "Log entry added."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
